c0c4c1ebe87c87316149810ddad74400aefcf064
MM4096 <85976396+MM4096@users.noreply.github.com>
2023-04-28 13:47:03 +1200

    added venv

---
+++ auth.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+
+def Login(auth, email, password):
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        return user
+    except requests.exceptions.HTTPError as err:
+        errorMessage = json.loads(err.args[1])["error"]["message"]
+        print(errorMessage)
+        if errorMessage == "INVALID_EMAIL":
+            print("Invalid email!")
+        elif errorMessage == "EMAIL_NOT_FOUND":
+            print("Email not found!")
+        elif errorMessage == "INVALID_PASSWORD":
+            print("Incorrect password!")
+        elif errorMessage == "USER_DISABLED":
+            print("User disabled!")
+        elif errorMessage == "TOO_MANY_ATTEMPTS_TRY_LATER":
+            print("Too many attempts! Try again later!")
+        return None
+
+
+def Register(auth, email, password):
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        return user
+    except requests.exceptions.HTTPError as err:
+        errorMessage = json.loads(err.args[1])["error"]["message"]
+        if errorMessage == "INVALID_EMAIL":
+            print("Invalid email!")
+        elif errorMessage == "WEAK_PASSWORD":
+            print("Weak password!")
+        elif errorMessage == "EMAIL_EXISTS":
+            print("Email already exists!")
+        return None
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from PyQt6.QtWidgets import QApplication, QWidget
-
 import sys
-app = QApplication(sys.argv)
-window = QWidget()
-window.show()
-app.exec()
+import pyrebase
+from auth import Login, Register
 
+# app = QApplication(sys.argv)
+# window = QWidget()
+# window.show()
+# app.exec()
+
+firebaseConfig = {
+    "apiKey": "AIzaSyDE6yjJa9JfNSasdkZ6qIGq62dEvDMTVSI",
+    "authDomain": "test-4fad1.firebaseapp.com",
+    "databaseURL": "https://test-4fad1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "storageBucket": "test-4fad1.appspot.com",
+    "messagingSenderId": "579594052865",
+    "appId": "1:579594052865:web:a9389d58d5fc5b5352a700"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+db = firebase.database()
+
+if __name__ == "__main__":
+    username = input("Enter your email: ")
+    password = input("Enter your password: ")
+
+    user = Register(auth, username, password)
