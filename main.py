3d94dfeb9331e5d150a32e90e31f6f5a3a74a64a
MM4096 <85976396+MM4096@users.noreply.github.com>
2023-05-02 15:09:43 +1200

    Updating layouts

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel
 import sys
 import pyrebase
 from auth import Login, Register
 
-# app = QApplication(sys.argv)
-# window = QWidget()
-# window.show()
-# app.exec()
 
 firebaseConfig = {
     "apiKey": "AIzaSyDE6yjJa9JfNSasdkZ6qIGq62dEvDMTVSI",
@@ -21,8 +18,31 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
 
-if __name__ == "__main__":
-    username = input("Enter your email: ")
-    password = input("Enter your password: ")
 
-    user = Register(auth, username, password)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Translator")
+
+        label = QLabel("Hello world!")
+
+        self.setFixedSize(QSize(400, 300))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(label)
+
+    def DoSomeStuff(self):
+        print("Hello world!")
+
+
+if __name__ == "__main__":
+    # username = input("Enter your email: ")
+    # password = input("Enter your password: ")
+    #
+    # user = Register(auth, username, password)
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
